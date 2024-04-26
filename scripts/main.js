7e659c10ed0d624f88a6cd6b1428fe13743d24e4
Maddox1231 <144711107+Maddox1231@users.noreply.github.com>
2024-04-26 16:09:13 +0000

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,14 @@ for (let i = 1; i <= 10; i++) {
 }
 
 
-console.log("countdown:");
+console.log("countdown");
 for (let i = 5; i >= 1; i--) {
     console.log(i);
 }
 console.log("blastoff");
 
 
-console.log("even numbers from 1 to 50:");
+console.log("even numbers from 1 to 50");
 for (let i = 2; i <= 50; i += 2) {
     console.log(i);
 }
@@ -22,10 +22,10 @@ let sum = 0;
 for (let i = 1; i <= 100; i++) {
     sum += i;
 }
-console.log("sum of 1 through 100:", sum);
+console.log("sum of 1 through 100", sum);
 
 
-console.log("first 10 factorials:");
+console.log("first 10 factorials");
 for (let i = 1; i <= 10; i++) {
     let factorial = 1;
     for (let j = 1; j <= i; j++) {
