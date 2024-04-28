e24467e23bd18589de82fe5a424898e5284941ec
Maddox1231 <144711107+Maddox1231@users.noreply.github.com>
2024-04-28 15:34:33 +0000

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ console.log("Task 2");
 for (let i = 5; i >= 1; i--) {
     console.log(i);
 }
-console.log("Blastoff");
+console.log("Blastoff!");
 
 
 console.log("Task 3");
@@ -18,7 +18,6 @@ for (let i = 1; i <= 50; i++)
     if (i % 2 === 0) {
         console.log(i);
     }
-
 
 console.log("Task 4");
 let sum = 0;
@@ -34,3 +33,37 @@ for (let i = 1; i <= 10; i++) {
     factorial *= i;
     console.log(factorial);
 }
+console.log("Task 6")
+let arr = [3, 1, 4, 1, 5];
+for (let i = 0; i <= arr.length - 1; i++) {
+   console.log(arr[i]);
+}
+
+console.log("Task 7");
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr.at(i));
+}
+
+console.log("Task 8");
+let cubeNumbers = [];
+for (let i = 1; i <= 10; i++) {
+    cubeNumbers.push(i ** 3);
+}
+console.log(cubeNumbers);
+
+console.log("Task 9");
+let fibonacciNumbers = [];
+fibonacciNumbers[0] = 0;
+fibonacciNumbers[1] = 1;
+for (let i = 2; i < 10; i++) {
+    fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+}
+console.log(fibonacciNumbers);
+
+console.log("Task 10");
+let reversedArr = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArr.push(arr[i]);
+}
+console.log(reversedArr);
+
