d37921cecb85a97bcc8af239fabf27117eb84aef
Maddox1231 <144711107+Maddox1231@users.noreply.github.com>
2024-05-05 18:47:04 +0000

    JS: creating dynamic content

--- scripts/main.js
+++ scripts/main.js
@@ -67,3 +67,14 @@ for (let i = arr.length - 1; i >= 0; i--) {
 }
 console.log(reversedArr);
 
+let colors = ["red","orange","yellow","green","blue","indigo","violet"];
+let ul = document.createElement("ul");
+for (let i = 0; i < colors.length; i++) {
+   let li = document.createElement("li");
+   li.append(colors[i]);
+   li.style.color = colors[i]
+   ul.appendChild(li);
+}
+
+
+document.body.appendChild(ul);
