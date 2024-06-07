42259ae99921b114434bbefc7e33c707c968c072
Maddox1231 <144711107+Maddox1231@users.noreply.github.com>
2024-06-07 15:31:35 +0000

    changes

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-const image = document.getElementById('myImage');
-
-image.addEventListener('click', () => {
-  image.style.transform = 'rotate(360deg)';
-  image.style.transition = 'transform 2s';
-});
